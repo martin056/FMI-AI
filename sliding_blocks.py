bfd0e5a279213c22429b8c86ce66c6745f043bb3
martin95056 <martinangelov056@gmail.com>
2017-10-29 20:48:09 +0200

    Add solution for 3x3

--- sliding_blocks.py
+++ sliding_blocks.py
@@ -1,38 +1,31 @@
 import random
 from copy import deepcopy
 
-"""
-g(x) - the steps I've made
-h1(x) - the sum of how many numbers are not on their place
-h2(x) - Manhattan distance with n = 1. We will use this!
-
-heuristic(x) = g(x) + h2(x)
-"""
-
 
 class Node:
-    def __init__(self, grid, heuristic, parent):
+    def __init__(self, grid, grid_score, parent):
         self.grid = grid
-        self.heuristic = heuristic
+        self.grid_score = grid_score
         self.parent = parent
 
-        self.path_heuristic = self.calculate_path_heuristic()
+        self.distance = self.calculate_distance()
 
-    def __str__(self):
-        return '\n'.join([str(row) for row in self.grid])
+    @property
+    def heuristic(self):
+        return self.grid_score
 
-    def calculate_path_heuristic(self):
-        result = self.heuristic
-        node = self.parent
+    def calculate_distance(self):
+        parent = self.parent
+        counter = 0
 
-        while node:
-            result += node.heuristic
-            node = node.parent
+        while parent is not None:
+            counter += 1
+            parent = parent.parent
 
-        return result
+        return counter
 
 
-class Table:
+class SlidingBlocks:
     MOVING_ELEMENT = 0
 
     def __init__(self, n):
@@ -40,16 +33,16 @@ class Table:
         self.max_element_value = n ** 2
         self.final_grid = self.generate_final_grid()
         self.grid = self.generate_random_grid()
-        # self.grid = [
-        #     [1,0,2],
-        #     [3,4,5],
-        #     [6,8,7]
-        # ]
 
-        self.distance = 0
         self.visited = []
         self.nodes = []
-        self.last_node = None
+
+    def is_solvable(self, grid):
+        moving_element_manhattan = self.manhattan(self.MOVING_ELEMENT, grid)
+        """
+        TODO: count permutations
+        """
+        return True
 
     def generate_final_grid(self):
         result = []
@@ -64,11 +57,15 @@ class Table:
     def generate_random_grid(self):
         result = []
         numbers = list(range(self.max_element_value))
+
         random.shuffle(numbers)
 
         for _ in range(1, self.given_number + 1):
             result.append(numbers[:self.given_number])
             numbers = numbers[self.given_number:]
+
+        if not self.is_solvable(result):
+            self.generate_random_grid()
 
         return result
 
@@ -133,25 +130,22 @@ class Table:
 
         return next_states
 
-    def update_distance(self):
-        self.distance += 1
-
-    def manhattan(self, grid):
+    def get_grid_score(self, grid):
         result = 0
 
         for number in range(0, self.max_element_value):
-            perfect_place = self.find_perfect_place(number)
-            place_in_grid = self.find_number(number, grid)
-
-            x_to_target = abs(perfect_place[0] - place_in_grid[0])
-            y_to_target = abs(perfect_place[1] - place_in_grid[1])
-
-            result += (x_to_target + y_to_target)
+            result += self.manhattan(number, grid)
 
         return result
 
-    def heuristic(self, grid):
-        return self.manhattan(grid)
+    def manhattan(self, number, grid):
+        perfect_place = self.find_perfect_place(number)
+        place_in_grid = self.find_number(number, grid)
+
+        x_to_target = abs(perfect_place[0] - place_in_grid[0])
+        y_to_target = abs(perfect_place[1] - place_in_grid[1])
+
+        return (x_to_target + y_to_target)
 
     def reorder_grid(self, number):
         """
@@ -168,8 +162,11 @@ class Table:
         return grid
 
     def get_best_possible_node(self):
-        nodes = set(self.nodes) - set(self.visited)
-        sorted_nodes = sorted(list(nodes), key=lambda node: node.path_heuristic)
+        for node in self.nodes:
+            if node.grid in [n.grid for n in self.visited]:
+                self.nodes.remove(node)
+
+        sorted_nodes = sorted(self.nodes, key=lambda node: node.heuristic)
 
         return sorted_nodes[0]
 
@@ -179,41 +176,45 @@ class Table:
         for move, target_number in possible_moves.items():
             if target_number:
                 grid = self.reorder_grid(target_number)
-                grid_heuristic = self.heuristic(grid)
+
+                grid_score = self.get_grid_score(grid)
                 parent = self.visited[-1]
 
                 node = Node(grid=grid,
-                            heuristic=grid_heuristic,
+                            grid_score=grid_score,
                             parent=parent)
 
-                self.nodes.append(node)
+                if node.grid not in [n.grid for n in self.visited]:
+                    self.nodes.append(node)
 
     def choose_next_node(self):
-        self.extend_nodes()
-
         best_node = self.get_best_possible_node()
 
-        while best_node in self.visited:
-            self.nodes.remove(best_node)
-            best_node = self.get_best_possible_node()
-
-        self.grid = best_node.grid
         self.visited.append(best_node)
+        self.grid = best_node.grid
 
-    def play_sliding_blocks(self):
+    def play(self):
         # Add root of the tree
         intiial_node = Node(grid=self.grid,
-                            heuristic=self.heuristic(self.grid),
+                            grid_score=self.get_grid_score(self.grid),
                             parent=None)
 
         self.visited.append(intiial_node)
         self.nodes.append(intiial_node)
 
         while self.grid != self.final_grid:
+            self.extend_nodes()
             self.choose_next_node()
-            self.update_distance()
+
+        winner = self.visited[-1]
+        parent = winner.parent
+        print("SOLVED")
+        print(winner.grid)
+        while parent is not None:
+            print(parent.grid)
+            parent = parent.parent
 
 
 if __name__ == '__main__':
-    table = Table(3)
-    table.play_sliding_blocks()
+    game = SlidingBlocks(4)
+    game.play()
